fef28c6a8e6bed0eb9976c22c0c5f0f5b8aab7d4
tayog <basallo.tayog@gmail.com>
2026-06-08 16:15:20 +0800

    feat: v1.1 made letter at picture one

---
+++ Picture 1 - Amazon Logo/amazon.asm
@@ -0,0 +1,74 @@
+.MODEL SMALL
+.STACK 100h
+
+; ============================================================
+; REUSABLE MACRO: draw_rectangle
+; ============================================================
+; Usage: draw_rectangle x_start, y_start, color, height, width
+; ============================================================
+draw_rectangle MACRO x, y, color, height, width
+    LOCAL row_loop, col_loop
+    MOV CX, x
+    MOV DX, y
+    MOV AL, color
+    MOV SI, height
+row_loop:
+    PUSH CX
+    MOV DI, width
+col_loop:
+    MOV AH, 0Ch
+    INT 10h
+    INC CX
+    DEC DI
+    JNZ col_loop
+    POP CX
+    INC DX
+    DEC SI
+    JNZ row_loop
+ENDM
+
+.CODE
+START:
+    MOV AX, 13h
+    INT 10h
+    
+    ; ===== WHITE BACKGROUND =====
+    draw_rectangle 0, 0, 15, 200, 320
+    
+
+    
+    ; ===== LETTER 'a' BASE ( =====
+    draw_rectangle 20, 30, 0, 45, 35
+    
+    ; ===== CARVE CENTER HOLE =====
+    draw_rectangle 30, 55, 15, 10, 10
+
+    ; left detail
+    draw_rectangle 20, 45, 15, 5, 10
+    draw_rectangle 20, 50, 15, 5, 5
+    draw_rectangle 20, 70, 15, 5, 5
+
+    ;bottom and right detail
+    draw_rectangle 40, 70, 15, 5, 5
+    draw_rectangle 50, 70, 15, 5, 5
+    draw_rectangle 50, 30, 15, 35, 5
+    draw_rectangle 45, 30, 15, 5, 5
+
+    ; ===== TOP OPENING =====
+    draw_rectangle 30, 40, 15, 10, 10
+    
+    ; ===== CARVE TOP CURVE =====
+    draw_rectangle 20, 30, 15, 5, 10
+    draw_rectangle 20, 35, 15, 5, 5
+    
+    ; Wait for key press
+    XOR AH, AH
+    INT 16h
+    
+    ; Return to text mode and exit
+    MOV AX, 3
+    INT 10h
+    MOV AX, 4C00h
+    INT 21h
+
+END START
